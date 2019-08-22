bb6476b42f8a99cd0ea9620ed5ba0232be2aaf93
Burhan Budak <budak.burhan@gmail.com>
2019-08-22 16:14:53 +0200

    0.1

--- script.js
+++ script.js
@@ -83,10 +83,7 @@ function parallax() {
     var rect = el.getBoundingClientRect();
   
     return (
-      // rect.top >= 0 &&
-      // rect.left >= 0 &&
-      // rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      // rect.right <= (window.innerWidth || document.documentElement.clientWidth) 
+      
       rect.top + (rect.height)/2 < window.innerHeight
     );
   }
